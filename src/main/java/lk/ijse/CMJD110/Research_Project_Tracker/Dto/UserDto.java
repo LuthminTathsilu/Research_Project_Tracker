338fec0621acbd18bbb8b50ea170712b09ef6476
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-02 20:51:11 +0530

    Full UserDto based on the role

---
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/Dto/UserDto.java
@@ -0,0 +1,12 @@
+package lk.ijse.CMJD110.Research_Project_Tracker.Dto;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+    private String id;
+    private String username;
+    private String password;
+    private String fullname;
+    private Role role;
+    private LocalDateTime createdAt;
+}
