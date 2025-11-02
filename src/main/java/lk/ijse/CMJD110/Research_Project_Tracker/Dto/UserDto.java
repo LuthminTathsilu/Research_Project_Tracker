8125a63d260887ab4c9f574fe4f6c1c66b083d03
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-02 21:33:01 +0530

    Save Principal Investigator

--- src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/Dto/UserDto.java
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/Dto/UserDto.java
@@ -1,8 +1,16 @@
 package lk.ijse.CMJD110.Research_Project_Tracker.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class UserDto {
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDto implements Serializable {
     private String id;
     private String username;
     private String password;
