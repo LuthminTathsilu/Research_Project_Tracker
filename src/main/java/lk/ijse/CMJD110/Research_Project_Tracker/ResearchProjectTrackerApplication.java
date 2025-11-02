f329875a9f35dca3f637376e7cbf79c9f414c375
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-02 19:59:19 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/ResearchProjectTrackerApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.CMJD110.Research_Project_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchProjectTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchProjectTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/CMJD110/Research_Project_Tracker/ResearchProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.CMJD110.Research_Project_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchProjectTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
