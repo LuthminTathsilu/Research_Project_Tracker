b81a0a46031382300603062478578653547885a8
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-06 12:49:28 +0530

    Enabling JPA Annotation in the main

--- src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/ResearchProjectTrackerApplication.java
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/ResearchProjectTrackerApplication.java
@@ -2,12 +2,15 @@ package lk.ijse.CMJD110.Research_Project_Tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ResearchProjectTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResearchProjectTrackerApplication.class, args);
+
+        SpringApplication.run(ResearchProjectTrackerApplication.class, args);
 	}
 
 }
