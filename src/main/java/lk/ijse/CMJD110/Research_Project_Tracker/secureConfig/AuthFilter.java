51d2bf7bf4415f4b58373bffa9697c1750b5fe2f
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-11 09:00:26 +0530

    Handling Auth filter

--- src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/secureConfig/AuthFilter.java
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/secureConfig/AuthFilter.java
@@ -5,23 +5,55 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.CMJD110.Research_Project_Tracker.Dao.secure.UserSecureDao;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.core.config.Order;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @Configuration
 @RequiredArgsConstructor
 @Order(1)
 public class AuthFilter extends OncePerRequestFilter {
+    private final JWTUtils jwtUtils;
+    private final UserSecureDao userSecureDao;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //Todo: IMPL
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            var token = getToken(request);
+            if(token != null && jwtUtils.validateToken(token)){
+                var userNameFromToken = jwtUtils.getUserNameFromToken(token);
+                var userDetails = userSecureDao.findByUsername(userNameFromToken);
+                var authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.get().getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        filterChain.doFilter(request,response);
     }
 
+    private String getToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")){
+            return authHeader.substring(7);
+        }
+        return null;
+    }
 }
