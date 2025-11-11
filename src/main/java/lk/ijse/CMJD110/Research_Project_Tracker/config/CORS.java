71241ddd5c04216ce0654b4f74d778ed805f06f1
LuthminTathsilu <luthmintathsilu@gmail.com>
2025-11-11 08:39:00 +0530

    Implementing web security config with the updated CORS

--- src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/config/CORS.java
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/config/CORS.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -12,19 +13,17 @@ import java.util.List;
 @Configuration
 public class CORS {
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter(){
-        var config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
-        config.setAllowedHeaders(List.of("Content-Type","Authorization"));
-        config.setAllowCredentials(true);
+    public CorsConfigurationSource corsConfig() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedHeaders(List.of("Content-Type","Authorization"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Authorization"));
 
         var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        var corsBean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-        corsBean.setOrder(0);
-        return corsBean;
-
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
 }
--- src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/secureConfig/WebSecurityConfig.java
+++ src/main/java/lk/ijse/CMJD110/Research_Project_Tracker/secureConfig/WebSecurityConfig.java
@@ -1,4 +1,65 @@
 package lk.ijse.CMJD110.Research_Project_Tracker.secureConfig;
 
+
+
+import lk.ijse.CMJD110.Research_Project_Tracker.config.CORS;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final AuthEntryPoint authEntryPoint;
+    private final AuthFilter authFilter;
+    private final CORS corsConfiguration;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var dap = new DaoAuthenticationProvider();
+        dap.setUserDetailsService(userDetailsService);
+        dap.setPasswordEncoder(passwordEncoder());
+        return dap;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+
+    }
+    @Bean
+    public SecurityFilterChain configuration(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors-> cors.configurationSource(corsConfiguration.corsConfig()))
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests( auth->
+                        auth.requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+
 }
